9f60e5e6f68f9abf9b55ff58f31a8c51721e5066
Harieo <harieo@hotmail.com>
2020-04-01 13:29:12 +0100

    Added QuackConnector implementation

--- src/main/java/uk/co/harieo/minigames/MinigamesCore.java
+++ src/main/java/uk/co/harieo/minigames/MinigamesCore.java
@@ -1,12 +1,30 @@
 package uk.co.harieo.minigames;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import uk.co.harieo.QuackConnector.QuackConnector;
 
 public class MinigamesCore extends JavaPlugin {
 
+	private static boolean isConnectorEnabled = false;
+
 	@Override
 	public void onEnable() {
+		isConnectorEnabled = Bukkit.getPluginManager().isPluginEnabled("QuackConnector");
+	}
 
+	public static void setAcceptingPlayers(boolean isAcceptingPlayers) {
+		if (isConnectorEnabled) {
+			QuackConnector.getInstance().setAcceptingPlayers(isAcceptingPlayers);
+			System.out.println("This server is " + (isAcceptingPlayers ? "now accepting players" : "no longer accepting players"));
+		}
+	}
+
+	public static void sendAllPlayersToFallbackServer() {
+		if (isConnectorEnabled) {
+			QuackConnector.getInstance().sendAllToFallback();
+		}
 	}
 
 }
