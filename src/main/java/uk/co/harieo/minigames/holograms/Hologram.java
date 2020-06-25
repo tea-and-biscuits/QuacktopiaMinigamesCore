e84c8ae2a254cee1d4005559e5c5cfcd222022e9
Harieo <harieo@hotmail.com>
2020-06-25 11:04:59 +0100

    Re-work of the Hologram API with simplification

--- src/main/java/uk/co/harieo/minigames/holograms/Hologram.java
+++ src/main/java/uk/co/harieo/minigames/holograms/Hologram.java
@@ -5,61 +5,112 @@ import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
+/**
+ * A class which handles spawning invisible named armour stands (holograms)
+ */
 public class Hologram {
 
+	private final List<String> lines = new ArrayList<>();
+	private final List<ArmorStand> stands = new ArrayList<>();
 	private Location location;
-	private List<String> lines;
-	private Map<Integer, ArmorStand> stands = new HashMap<>();
 
-	public Hologram(Location location, List<String> lines) {
-		this.location = location;
-		this.lines = lines;
-	}
-
+	/**
+	 * @return the location which represents the X and Z coordinates of where this hologram will spawn. The Y axis is
+	 * off-set from the base location due to an armour stand being a ~2 block high entity
+	 */
 	public Location getLocation() {
 		return location;
 	}
 
+	/**
+	 * Sets the location that the armour stand will spawn at
+	 *
+	 * @param location which the armour stand should spawn at, not compensating for the Y axis height
+	 * @return this instance
+	 */
+	public Hologram setLocation(Location location) {
+		this.location = location.clone();
+		return this;
+	}
+
+	/**
+	 * @return the lines of text to be displayed
+	 */
 	public List<String> getLines() {
 		return lines;
 	}
 
-	public void setLines(List<String> lines) {
-		this.lines = lines;
+	/**
+	 * Adds a line of text to be displayed to the list
+	 *
+	 * @param line of text to be displayed
+	 */
+	public void addLine(String line) {
+		lines.add(line);
 	}
 
+	/**
+	 * Removes the line in the list at the specified index, if applicable
+	 *
+	 * @param index to remove the line at in the list
+	 */
+	public void removeLine(int index) {
+		if (index < lines.size()) {
+			lines.remove(index);
+		}
+	}
+
+	/**
+	 * Sets the line in the list at the specified index, if applicable
+	 *
+	 * @param index of the line in the list to be replaced
+	 * @param line to set at the specified index
+	 */
+	public void setLine(int index, String line) {
+		if (index < lines.size()) {
+			lines.set(index, line);
+		}
+	}
+
+	/**
+	 * Spawns the armour stand entities and formats them based on displayed lines. This method requires that the
+	 * location has been set and there is at least 1 line to display.
+	 */
 	public void updateLines() {
-		for (int i = 0; i < lines.size(); i++) {
-			String line = ChatColor.translateAlternateColorCodes('&', lines.get(i));
-			double yAxisOffset = (0.25 * i) - 1;
-			if (stands.containsKey(i)) {
-				ArmorStand stand = stands.get(i);
-				stand.setCustomName(line);
-				stand.teleport(location.clone().add(0, yAxisOffset, 0));
-			} else if (!line.isEmpty()) { // No point spawning an entity that will have no purpose
-				ArmorStand newStand = (ArmorStand) Objects.requireNonNull(location.getWorld())
-						.spawnEntity(location.clone().add(0, yAxisOffset, 0), EntityType.ARMOR_STAND);
-				newStand.setGravity(false);
-				newStand.setVisible(false);
-				newStand.setCustomNameVisible(true);
-				newStand.setInvulnerable(true);
-				newStand.setCustomName(line);
-				stands.put(i, newStand);
+		clearHologram();
+		if (!lines.isEmpty() && location != null) {
+			int lineCount = lines.size();
+			for (int i = 0; i < lineCount; i++) {
+				String line = ChatColor.translateAlternateColorCodes('&', lines.get(i));
+				double yAxisOffset = (0.25 * (lineCount - i)) - 1;
+
+				if (i >= stands.size()) { // This armor stand hasn't been spawned yet
+					ArmorStand newStand = (ArmorStand) Objects.requireNonNull(location.getWorld())
+							.spawnEntity(location.clone().add(0, yAxisOffset, 0), EntityType.ARMOR_STAND);
+					newStand.setGravity(false);
+					newStand.setVisible(false);
+					newStand.setCustomNameVisible(true);
+					newStand.setInvulnerable(true);
+					newStand.setCustomName(line);
+					stands.add(newStand);
+				} else {
+					ArmorStand stand = stands.get(i);
+					stand.setCustomName(line);
+					stand.teleport(location.clone().add(0, yAxisOffset, 0));
+				}
 			}
 		}
 	}
 
-	void destroyHologram() {
-		for (ArmorStand stand : stands.values()) {
+	/**
+	 * Removes all spawned armour stands, effectively deleting any displayed holograms
+	 */
+	private void clearHologram() {
+		for (ArmorStand stand : stands) {
 			stand.remove();
 		}
-
-		lines.clear();
 		stands.clear();
 	}
 
